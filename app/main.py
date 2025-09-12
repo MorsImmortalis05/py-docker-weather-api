70590212446e6823d4314d392bfbe1b8a2f99635
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-09-12 20:44:01 +0300

    wrote main.py and added dockerfile, requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
+
+
+def get_weather() -> dict:
+    API_KEY=os.environ.get("API_KEY")
+    FILTERING = "Paris"
+    URL = f"http://api.weatherapi.com/v1/current.json?{API_KEY}&q={FILTERING}"
+    response = requests.get(URL)
+    data = response.json()
+    return data
 
 
 if __name__ == "__main__":
