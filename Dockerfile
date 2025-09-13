f4048492dd1d8aba1f6089f74f0d729be47652da
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-09-13 11:05:57 +0300

    sol

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11
+FROM python:3.11-slim
 LABEL maintainer="liliia"
 
 ENV PYTHONUNBUFFERED=1
--- app/main.py
+++ app/main.py
@@ -2,13 +2,28 @@ import os
 import requests
 
 
-def get_weather() -> dict:
+def validate(parameter):
+    if parameter is None:
+        raise ValueError(f"Requested data was not found in the response.")
+
+def get_weather():
     API_KEY=os.environ.get("API_KEY")
     FILTERING = "Paris"
-    URL = f"http://api.weatherapi.com/v1/current.json?{API_KEY}&q={FILTERING}"
+    URL = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={FILTERING}"
     response = requests.get(URL)
     data = response.json()
-    return data
+    current = data.get("current")
+    validate(current)
+    temperature = current.get("temp_c")
+    validate(temperature)
+    last_updated = current.get("last_updated")
+    validate(last_updated)
+    condition = current.get("condition")
+    validate(condition)
+    condition_text = condition.get("text")
+    validate(condition_text)
+    print(f"Paris/France {last_updated} "
+          f"Weather: {temperature} Celsius, {condition_text}")
 
 
 if __name__ == "__main__":
